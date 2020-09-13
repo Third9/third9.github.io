c719024a919cb10b02004e63578a8322d0a07ea9
Hyeock Kwon <dev.third9@gmail.com>
2020-09-13 17:24:26 +0900

    -. .vscode 명치 ignore 처리 -. style 수정 : TOC 관련 hover 처리 : TOC 긴 문장에 대해서 줄임 출력되도록 수정 및 helptext 나오게 수정 : chrome, safari에서 code block의 line-height가 다른 문제 수정

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,58 +1,67 @@
 // @flow strict
-import React from 'react';
-import { Link } from 'gatsby';
-import Author from './Author';
-import Comments from './Comments';
-import Content from './Content';
-import Meta from './Meta';
-import Tags from './Tags';
-import styles from './Post.module.scss';
-import type { Node } from '../../types';
-import kebabCase from 'lodash/kebabCase';
+import React from "react";
+import { Link } from "gatsby";
+import kebabCase from "lodash/kebabCase";
+import Author from "./Author";
+import Comments from "./Comments";
+import Content from "./Content";
+import Meta from "./Meta";
+import Tags from "./Tags";
+import styles from "./Post.module.scss";
+import type { Node } from "../../types";
 
 type Props = {
-  post: Node
+  post: Node,
 };
 
 const Post = ({ post }: Props) => {
   const { html } = post;
   const { tagSlugs, slug } = post.fields;
   const { tags, title, date } = post.frontmatter;
+  console.log("post >>> ", post);
   const tableOfContents = (
-      <ul className={styles['post__toc-list']}>
-        <li>
-          <label>{title}</label>
-        </li>
-        {post &&
-          post.headings.map(header => (
-            <li className={styles['post__toc-list-item']} key={header.value} style={{ paddingLeft: `${header.depth - 1}rem` }}>
-              <Link to={`${slug}#${kebabCase(header.value)}`} className={styles['post__toc-list-item-link']} >
-                {header.value}
-              </Link>
-            </li>
-          ))}
-      </ul>
-    )
+    <ul className={styles["post__toc-list"]}>
+      <li>
+        <label>{title}</label>
+      </li>
+      {post &&
+        post.headings.map((header) => (
+          <li
+            className={styles["post__toc-list-item"]}
+            key={header.value}
+            title={header.value}
+            style={{ paddingLeft: `${header.depth - 1}rem` }}
+          >
+            <Link
+              to={`${slug}#${kebabCase(header.value)}`}
+              className={styles["post__toc-list-item-link"]}
+            >
+              {header.value}
+            </Link>
+          </li>
+        ))}
+    </ul>
+  );
 
   return (
-    <div className={styles['post']}>
-      <Link className={styles['post__home-button']} to="/">All Articles</Link>
+    <div className={styles["post"]}>
+      <Link className={styles["post__home-button"]} to="/">
+        All Articles
+      </Link>
 
-      <div className={styles['post__toc']} >
-        {tableOfContents}
-      </div>
+      <div className={styles["post__toc"]}>{tableOfContents}</div>
 
-      <div className={styles['post__content']}>
+      <div className={styles["post__content"]}>
         <Content body={html} title={title} />
       </div>
 
-      <div className={styles['post__footer']}>
+      <div className={styles["post__footer"]}>
         <Meta date={date} />
         {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs} />}
         <Author />
       </div>
 
-      <div className={styles['post__comments']}>
+      <div className={styles["post__comments"]}>
         <Comments postSlug={slug} postTitle={post.frontmatter.title} />
       </div>
     </div>
