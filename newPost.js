3c76bb239d3655fae3a30d9d795ade0b8af407c3
Hyeock Kwon <dev.third9@gmail.com>
2021-03-07 11:37:40 +0900

    -. lint와 prettier 설정 추가 : 그에 따른 관련 코드들 lint 규칙 적용 -. package 정보등 일부 정보 수정

--- newPost.js
+++ newPost.js
@@ -1,24 +1,25 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const NewPost = () => {
-    let datetime = new Date().toISOString();
-    let today = datetime.split('T')[0];
-    const filePath = './content/posts/';
-    const fileName = today + '---NEW-POST.md'
-    const content = "---\n"
-                    + "title: 'NEW POST'\n"
-+ `date: ${datetime} \n`
-+ "template: \"post\"\n"
-+ "draft: false\n"
-+ "slug: \"/posts/NEW_POST/\"\n"
-+ "category: \"develop\"\n"
-+ "tags:\n"
-+ "  - \"tag\"\n"
-+ "description: \n"
-+  "---"
-    fs.writeFile(filePath+fileName, content, (err) => {
-        if (err) throw err;
-        console.log('Created New Post Template');
-    })
-}
-NewPost()
+  const datetime = new Date().toISOString();
+  const today = datetime.split("T")[0];
+  const filePath = "./content/posts/";
+  const fileName = `${today}---NEW-POST.md`;
+  const content = `---
+title: 'NEW POST'
+date: ${datetime}
+template: "post"
+draft: false
+slug: "/posts/NEW_POST/"
+category: "develop"
+tags:
+  - "tag"
+description:
+---`;
+
+  fs.writeFile(filePath + fileName, content, err => {
+    if (err) throw err;
+    console.log("Created New Post Template");
+  });
+};
+NewPost();
